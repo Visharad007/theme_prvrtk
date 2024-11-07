caf51592f7b884ce1c4d81c8fa97f58f30b1ee53
Visharad007 <117433856+Visharad007@users.noreply.github.com>
2024-11-07 16:03:22 +0530

    Update setup.py

--- theme_pravartak/setup.py
+++ theme_pravartak/setup.py
@@ -1,19 +1,17 @@
-from setuptools import setup, find_packages
-
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
-
-# get version from __version__ variable in whitetheme_v13/__init__.py
-from forest_theme import __version__ as version
+version = '0.0.1'
 
 setup(
-	name='theme_pravartak',
-	version=version,
-	description='theme for erpnext',
-	author='Visharad',
-	author_email='vish@gmail.com',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
+    name='theme_pravartak',
+    version=version,
+    description='A custom theme for ERPNext',
+    author='Visharad',
+    author_email='vish@gmail.com',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'frappe',
+    ],
+    include_package_data=True,
+    data_files=[('assets/theme_pravartak/css', ['theme_pravartak/public/css/theme_pravartak.css']),
+                
 )
